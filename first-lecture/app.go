4870ea4d36de75854b1013453487890be17d96bf
Abdurauf <abdurauf@Abduraufs-MacBook-Pro.local>
2024-09-09 23:11:21 +0500

    Invest calculator

---
+++ first-lecture/app.go
@@ -0,0 +1,11 @@
+package main // start of execution of go
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World")
+}
+	
+// to run code type -> go run app.go
+// go mod init example.com/first-app --> to converto module
+// go build --> make .exe file 
---
+++ seconde-lecture/go.mod
@@ -0,0 +1,3 @@
+module example/investment-calc
+
+go 1.23.1
