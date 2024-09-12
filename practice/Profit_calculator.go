21a17b1a6f7e0a26f3e64d32928bc08cbed8473f
Abdurauf <abdurauf@Abduraufs-MacBook-Pro.local>
2024-09-12 07:02:30 +0500

    Functions

--- practice/Profit_calculator.go
+++ practice/Profit_calculator.go
@@ -9,21 +9,34 @@ func main()  {
 	var expenses float64
 	var tax_rate float64
 
-	fmt.Print("Enter revenue amount: ")
-	fmt.Scan(&revenue)
+	revenue = outputText("Enter revenue amount: ",revenue)
+	expenses = outputText("Enter expenses: ",expenses)
+	tax_rate = outputText("Enter tax_rate: ",tax_rate)
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	ebt,profit,ration := calculateProfit(revenue,expenses,tax_rate)
 
-	fmt.Print("Enter tax_rate: ")
-	fmt.Scan(&tax_rate)
+	febt := formattedText("EBT",ebt)
+	fp := formattedText("PROFIT",profit)
+	fr := formattedText("RATIO",ration)
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - tax_rate / 100)
-	ration := ebt / profit
-	
-	fmt.Println("EBT: ",ebt)
-	fmt.Println("PROFIT: ",profit)
-	fmt.Println("RATIO: ",ration)
+	fmt.Print(febt,fp,fr)
+}
 
+func outputText(text string, value float64) (float64) {
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
+}
+
+func calculateProfit(revenue,expenses,tax_rate float64) (ebt float64, profit float64, ratio float64){
+	ebt = revenue - expenses
+	profit = ebt * (1 - tax_rate / 100)
+	ratio = ebt / profit
+
+	return ebt,profit,ratio
+}
+
+func formattedText(text string, value float64) (formattedValue string){
+	formattedValue = fmt.Sprintf(text + ": %.1f\n", value)
+	return
 }
--- seconde-lecture/investment_calculator.go
+++ seconde-lecture/investment_calculator.go
@@ -5,26 +5,45 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main(){
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5;
+	 
+	investmentAmount = outputText("Enter investment amount: ",investmentAmount)
+	expectedReturnRate = outputText("Enter Expected Return Rate: ",expectedReturnRate)
+	years = outputText("Enter years: ",years)
 
-	fmt.Print("Enter investment amount: ")
-	fmt.Scan(&investmentAmount) // & is pointer allows Scan to populate variable (not working with constant)
+	futureValue,futureRealValue := calculateFutureValue(investmentAmount,expectedReturnRate,years)
 
-	fmt.Print("Enter Expected Return Rate: ")
-	fmt.Scan(&expectedReturnRate)
-	
-	fmt.Print("Enter years: ")
-	fmt.Scan(&years)
-
-	futureValue :=  investmentAmount * math.Pow((1 + expectedReturnRate / 100),years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	// ** More formatted way display formatted text
+	formattedFV := fmt.Sprintf("Future value: %.1f\n",futureValue) 
+	formattedRFV := fmt.Sprintf("Future value (adjusted value): %.1f\n",futureRealValue)
 
 // ** OUTPUT INFORMATION
 	// fmt.Println("Future value: ",futureValue)
-	fmt.Printf("Future value: %v\nFuture value (adjusted value): %v",futureValue,futureRealValue)
+	// fmt.Printf("Future value: %.1f\nFuture value (adjusted value): %.1f",futureValue,futureRealValue)
+	
+// ** I can add more space in here
+	// fmt.Printf(`Future value: %.1f Future value (adjusted value): %.1f`,futureValue,futureRealValue) 
 	// fmt.Println("Future value (adjusted value):",futureRealValue)
+	fmt.Print(formattedFV,formattedRFV)
+}
+
+// & is pointer allows Scan to populate variable (not working with constant)
+func outputText(text string, value float64)(float64){
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
+}
+
+// by doing (fv float64,rfv float64) go create by him self variables in function
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64 ) (fv float64,rfv float64){
+	fv = investmentAmount * math.Pow((1 + expectedReturnRate / 100),years)
+	rfv = fv / math.Pow(1 + inflationRate / 100, years)
+	return fv,rfv
+	// ** alternative way to return values
+	// return
 }
