dc4eee8c583b51222adc65cba681de6ee55977c4
Abdurauf7 <Abdurauf98@mail.ru>
2025-02-05 16:38:59 +0500

    Practice

--- practice/Profit_calculator.go
+++ practice/Profit_calculator.go
@@ -1,39 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
-func main()  {
-	revenue := getUserInput("Enter revenue amount: ")
-	expenses := getUserInput("Enter expenses: ")
-	tax_rate := getUserInput("Enter tax_rate: ")
+func main() {
+	revenue, err1 := getUserInput("Enter revenue amount: ")
 
-	ebt,profit,ration := calculateFinancials(revenue,expenses,tax_rate)
+	expenses, err2 := getUserInput("Enter expenses: ")
 
-	febt := formattedText("EBT",ebt)
-	fp := formattedText("PROFIT",profit)
-	fr := formattedText("RATIO",ration)
+	tax_rate, err3 := getUserInput("Enter tax_rate: ")
 
-	fmt.Print(febt,fp,fr)
+	if err1 != nil || err2 != nil || err3 != nil {
+		fmt.Println(err1)
+		return
+		// panic(err1)
+	}
+
+	ebt, profit, ration := calculateFinancials(revenue, expenses, tax_rate)
+
+	febt := formattedText("EBT", ebt)
+	fp := formattedText("PROFIT", profit)
+	fr := formattedText("RATIO", ration)
+
+	fmt.Print(febt, fp, fr)
+
+	writeCalculationToFile(ebt, profit, ration)
 }
 
-func getUserInput(text string) float64 {
+func writeCalculationToFile(ebt, profit, ration float64) {
+	calculationResult := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRation: %.3f\n", ebt, profit, ration)
+	os.WriteFile("calculation.txt", []byte(calculationResult), 0644)
+
+}
+
+func getUserInput(text string) (float64, error) {
 	var userInput float64
 	fmt.Print(text)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("invalid value, please input again")
+
+	}
+	return userInput, nil
 }
 
-func calculateFinancials(revenue,expenses,tax_rate float64) (ebt float64, profit float64, ratio float64){
+func calculateFinancials(revenue, expenses, tax_rate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expenses
-	profit = ebt * (1 - tax_rate / 100)
+	profit = ebt * (1 - tax_rate/100)
 	ratio = ebt / profit
 
-	return ebt,profit,ratio
+	return ebt, profit, ratio
 }
 
-func formattedText(text string, value float64) (formattedValue string){
-	formattedValue = fmt.Sprintf(text + ": %.1f\n", value)
+func formattedText(text string, value float64) (formattedValue string) {
+	formattedValue = fmt.Sprintf(text+": %.1f\n", value)
 	return formattedValue
 }
