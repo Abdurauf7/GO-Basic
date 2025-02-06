84ef2dc2e0e3205e7ac36426aaca65a18b7e16f1
Abdurauf7 <Abdurauf98@mail.ru>
2025-02-06 10:01:09 +0500

    structData

---
+++ section:5 Structs & Custom Types/app.go
@@ -0,0 +1,44 @@
+package main
+
+// Structs - grouping data & function into collections
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(appUser)
+}
+
+func outpuUserDetails(appUser any) string {
+	fmt.Println(appUser.firstName, appUser.lastName, appUser.birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
