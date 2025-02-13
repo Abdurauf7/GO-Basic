39f3b7bd40dccd2af7b25e488399f516507b233d
Abdurauf7 <Abdurauf98@mail.ru>
2025-02-13 10:52:57 +0500

    file storage

--- section_9_Practice_Price_Calculator/app.go
+++ section_9_Practice_Price_Calculator/app.go
@@ -1,23 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"section9/price_calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 
-		taxIncludedPrice := make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxIncludedPrice[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrice
 	}
-
-	fmt.Println(result)
 
 }
